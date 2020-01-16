6131fecf4821151b1d1e4137ee3c86b7938a016c
Kuu Miyazaki <miyazaqui@gmail.com>
2020-01-16 12:25:33 +0900

    Save all variants' playlists locally whenever invalid playlists are found

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const {URL} = require('url');
 const {Transform} = require('stream');
 const hlx = require('hlx-lib');
 const validUrl = require('valid-url');
 const timestamp = require('time-stamp');
+const fetch = require('node-fetch');
+const date = require('date-and-time');
 
 const srcUrl = process.argv[2];
 
@@ -17,9 +22,52 @@ function printErr(msg) {
   console.error(`${timestamp.utc('YYYY-MM-DD HH:mm:ss')} ${msg}`);
 }
 
+function createTmpDir(root, suffix = '') {
+  const dirName = `${date.format(new Date(), 'YYYY-MM-DD_HHmmss', true)}${suffix}`;
+  fs.mkdirSync(`${root}/${dirName}`, {recursive: true});
+  return dirName;
+}
+
+function resolveUrl(url, relativePath) {
+  const base = new URL(url);
+  base.pathname = path.resolve(base.pathname, relativePath);
+  return base.href;
+}
+
+function storeVariantData(dirPath, uri) {
+  const url = new URL(uri);
+  const fileName = path.basename(url.pathname);
+  const filePath = path.resolve(dirPath, fileName);
+  return fetch(uri)
+    .then(res => res.text())
+    .then(data => {
+      fs.writeFileSync(filePath, data);
+    })
+    .catch(err => err.stack);
+}
+
 class DurationChecker extends Transform {
   constructor() {
     super({objectMode: true});
+    this.variantUrls = [];
+    this.dirPath = `${__dirname}/${createTmpDir(__dirname)}`;
+  }
+
+  _extractVariantUrls({uri: baseUri, variants}) {
+    this.variantUrls = [];
+    for (const variant of variants) {
+      this.variantUrls.push(resolveUrl(baseUri, variant.uri));
+    }
+  }
+
+  _storeVariants(suffix) {
+    const dirPath = `${this.dirPath}/${createTmpDir(this.dirPath, suffix)}`;
+    const promiseList = [];
+    for (const uri of this.variantUrls) {
+      const promise = storeVariantData(dirPath, uri);
+      promiseList.push(promise);
+    }
+    return Promise.all(promiseList).then(() => dirPath);
   }
 
   _transform(data, _, cb) {
@@ -27,6 +75,7 @@ class DurationChecker extends Transform {
       if (data.isMasterPlaylist) {
         print(`Loading master playlist.`);
         print(`\tURI: ${data.uri}`);
+        this._extractVariantUrls(data);
         return cb(null, data);
       }
       print(`Loading media playlist.`);
@@ -42,6 +91,9 @@ class DurationChecker extends Transform {
           printErr('--- Contents of .m3u8 file: Start ---');
           printErr(data.source);
           printErr('--- Contents of .m3u8 file: End ---');
+          this._storeVariants('_invalid-duration').then(filePath => {
+            printErr(`All .m3u8 file are stored at: ${filePath}`);
+          });
         }
       }
     }
